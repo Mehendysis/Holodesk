8661cff201a3c3821e437b7c245e86630b9ba0ad
Remiam <rallarm@gmail.com>
2023-03-28 11:23:59 -0400

    UI minimal integration

--- renderers/OpenGL/OpenGLWindow.cpp
+++ renderers/OpenGL/OpenGLWindow.cpp
@@ -2,17 +2,48 @@
 #include "OpenGLWindow.h"
 #include <Window.h>
 #include <SDL.h>
+#include <imgui_impl_sdl2.h>
 
 void OpenGLWindow::GetWindowSize(int* width, int* height) const 
 {
     SDL_GetWindowSize(m_sdlWindow, width, height);
 }
 
+void OpenGLWindow::SQLEvent(Window* window)
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        ImGui_ImplSDL2_ProcessEvent(&event);
+
+        if (event.type == SDL_QUIT)
+        {
+            window->Quit();
+        }
+        else if (event.type == SDL_WINDOWEVENT)
+        {
+            if (event.window.event == SDL_WINDOWEVENT_MAXIMIZED)
+            {
+                int screenWidth, screenHeight;
+                SDL_GetWindowSize(m_sdlWindow, &screenWidth, &screenHeight);
+                SDL_SetWindowSize(m_sdlWindow, screenWidth, screenHeight);
+            }
+            // Handle window resize events
+            else if (event.window.event == SDL_WINDOWEVENT_RESIZED)
+            {
+                int screenWidth, screenHeight;
+                SDL_GetWindowSize(m_sdlWindow, &screenWidth, &screenHeight);
+                ImGuiIO& io = ImGui::GetIO();
+                io.DisplaySize = ImVec2((float)screenWidth, (float)screenHeight);
+            }
+        }
+    }
+}
 
 void OpenGLWindow::Create()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    m_sdlWindow = SDL_CreateWindow("OpenGL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width_, height_, SDL_WINDOW_OPENGL);
+    m_sdlWindow = SDL_CreateWindow("OpenGL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width_, height_, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
     SDL_GL_CreateContext(m_sdlWindow);
 }
 
